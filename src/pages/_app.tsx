221ee5d2168b5600d28ed33ff6618764fbe5b510
Adil Khojabergenov <adil.khojabergenov@convexitydmcc.com>
2023-09-06 16:25:09 +0600

    Styles to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { Layout } from '@/shared';
 import HydrationZustand from '@/shared/ui/HydrationZustand/HydrationZustand';
 import { AppProps } from 'next/app';
+import '../shared/styles/app.scss';
 
 export const Application = ({ Component, pageProps }: AppProps) => (
   <HydrationZustand>
--- src/shared/ui/Layout/Layout.tsx
+++ src/shared/ui/Layout/Layout.tsx
@@ -4,7 +4,6 @@ import { QueryClient } from '@tanstack/query-core';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactNode } from 'react';
 import { Layout as AntLayout } from 'antd';
-import '../../styles/app.scss';
 import styles from './Layout.module.scss';
 
 const { Content } = AntLayout;
